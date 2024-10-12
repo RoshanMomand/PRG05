25a578d2b80488d3c0cf900f0d82f39caad90a61
roshanmomand <1028086@hr.nl>
2024-10-12 23:56:26 +0200

    Changing back to normal

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+
+use App\Http\Controllers\BlogsController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Route::get('/blogs', function () {
+//    return view('blogs');
+//})->name('all.blogs');
+//
+
+Route::get('/blogs', [BlogsController::class, 'index'])->name('all.blogs');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
